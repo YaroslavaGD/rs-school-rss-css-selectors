1ec43172366863eeed88e9aae3e3446bf0050eb1
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-06-15 21:41:44 +0200

    feat: add basic webpack-settings

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const test = (a, b) => a * b;
-
+import './index.html';
+import './index.scss';
+import sushi from './img/sushi-1.svg';
+import { test, testsum } from './modules/calc';
+const sushiWrap = document.querySelector('.sushi');
+const img = new Image();
+img.src = sushi;
+sushiWrap.append(img);
 console.log(test(2, 4));
+console.log(testsum(2, 4));
---
+++ src/modules/calc.js
@@ -0,0 +1,2 @@
+export const test = (a, b) => a * b;
+export const testsum = async (a, b) => a + b;
